b24109860a226311dffa760be2b69458319e261c
Shuai Lou <shuai.lou@turbolinux.com.cn>
2017-04-27 16:32:23 +0800

    postfix

--- utils/init.go
+++ utils/init.go
@@ -5,15 +5,18 @@ import (
 	"errors"
 
 	"github.com/astaxie/beego/httplib"
+	"github.com/astaxie/beego/logs"
 )
 
 func InitMenuBar() error {
+	logs.Info("Init menu bar")
 	token, err := GetAccessToken()
 	if err != nil {
+		logs.Error(err)
 		return err
 	}
 
-	menu := ` { "button":[ {	"type":"click", "name":"今日网签", "key":"V1001_TODAY_SIGN" }] }`
+	menu := `{ "button":[ {"type":"click", "name":"今日网签", "key":"V1001_TODAY_SIGN" }] }`
 
 	req := httplib.Post("https://api.weixin.qq.com/cgi-bin/menu/create")
 	req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
@@ -23,6 +26,7 @@ func InitMenuBar() error {
 	var res WXResponse
 	err = req.ToJSON(&res)
 	if res.ErrCode != 0 {
+		logs.Error(res.ErrMsg)
 		return errors.New(res.ErrMsg)
 	}
 
